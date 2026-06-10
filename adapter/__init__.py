23ec0749f3531f1cf060d19285896f0a9abcc50f
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2026-06-10 09:44:39 +0700

    fix

--- adapter/__init__.py
+++ adapter/__init__.py
@@ -1,4 +1,4 @@
 
-from .projection import ImageProjModel, Resampler, Resampler2
+from .projection import ImageProjModel, Resampler
 from .model import MSAdapter
 from .attention_processor import AttnProcessor2_0, MaskedIPAttnProcessor2_0
